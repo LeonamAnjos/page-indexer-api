c441f1bf277dce2207d6195aa9d670d2df5cb7d7
Leonam Anjos <leonam.ricardo@gmail.com>
2018-04-02 19:09:43 +1000

    Improve flow when url already exists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,12 @@ class PagesController < ApplicationController
   # POST /pages
   def create
     page = Page.new(resource_params)
+
+    if Page.exists?(url: page.url)
+      jsonapi_render_errors json: error_url_already_exists, status: :unprocessable_entity
+      return
+    end
+
     content = open(page.url)
     parser = HtmlParser.new(content)
     page.h1 = parser.contents_of_tag('h1')
@@ -24,4 +30,17 @@ class PagesController < ApplicationController
       jsonapi_render_errors json: page, status: :unprocessable_entity
     end
   end
+
+  private
+
+    def error_url_already_exists
+      [{
+        title: "has already been taken",
+        detail: "Url has already been taken",
+        id: "url#taken",
+        code: "100",
+        source: { pointer: "/data/attributes/url" },
+        status: "422"
+      }]
+    end
 end
